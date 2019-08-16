fc04b6268018f279aa63d556e2cfacde880c652a
Sximsing <3010069635@qq.com>
2019-08-16 17:38:11 +0800

    commit

--- src/main/java/com/jeeplus/modules/wxapi/jeecg/qywx/api/message/vo/TextCard.java
+++ src/main/java/com/jeeplus/modules/wxapi/jeecg/qywx/api/message/vo/TextCard.java
@@ -1,13 +1,14 @@
 package com.jeeplus.modules.wxapi.jeecg.qywx.api.message.vo;
 
-public class Text {
-    private String touser;//成员ID列表（消息接收者，多个接收者用‘|’分隔，最多支持1000个）。特殊情况：指定为@all，则向关注该企业应用的全部成员发送
+public class TextCard {
+
+    private String touser; //成员ID列表（消息接收者，多个接收者用‘|’分隔，最多支持1000个）。特殊情况：指定为@all，则向关注该企业应用的全部成员发送
     private String toparty;//部门ID列表，多个接收者用‘|’分隔，最多支持100个。当touser为@all时忽略本参数
-    private String totag;//标签ID列表，多个接收者用‘|’分隔。当touser为@all时忽略本参数
+    private String totag;  //标签ID列表，多个接收者用‘|’分隔。当touser为@all时忽略本参数
     private String msgtype;//消息类型，此时固定为：text （支持消息型应用跟主页型应用）
-    private int agentid;//企业应用的id，整型。可在应用的设置页面查看
-    private TextEntity text;//消息内容实体
-    private String safe;//	表示是否是保密消息，0表示否，1表示是，默认0
+    private int agentid;   //企业应用的id，整型。可在应用的设置页面查看
+    private TextCardEntity textcard;// 消息卡片内容实体
+    private String enable_id_trans;// 表示是否开启id转译，0表示否，1表示是，默认0
 
     public String getTouser() {
         return touser;
@@ -49,20 +50,19 @@ public class Text {
         this.agentid = agentid;
     }
 
-    public TextEntity getText() {
-        return text;
+    public TextCardEntity getTextcard() {
+        return textcard;
     }
 
-    public void setText(TextEntity text) {
-        this.text = text;
+    public void setTextcard(TextCardEntity textcard) {
+        this.textcard = textcard;
     }
 
-    public String getSafe() {
-        return safe;
+    public String getEnable_id_trans() {
+        return enable_id_trans;
     }
 
-    public void setSafe(String safe) {
-        this.safe = safe;
+    public void setEnable_id_trans(String enable_id_trans) {
+        this.enable_id_trans = enable_id_trans;
     }
-
 }
