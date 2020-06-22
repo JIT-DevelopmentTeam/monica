75b80757d8d814b4dd32f3904af40465c6c727be
Vigny <944319975@qq.com>
2020-06-22 11:33:41 +0800

    Vigny

--- src/main/java/com/jeeplus/modules/wxapi/jeecg/qywx/api/base/JwParamesAPI.java
+++ src/main/java/com/jeeplus/modules/wxapi/jeecg/qywx/api/base/JwParamesAPI.java
@@ -10,17 +10,17 @@ public class JwParamesAPI {
 	// 随机戳
 	public static String encodingAESKey = "b2rxXq7GMymOskwpkMnwKPctb6ySWnmDQVIu7q0lKOW";  
 	 //你的企业号ID
-	public static String corpId = "ww49c384af1f4dac63";
+	public static String corpId = "ww2855e7a6c692c93b";
 	// 管理组的凭证密钥，每个secret代表了对应用、通讯录、接口的不同权限；不同的管理组拥有不同的secret
 	public static String secret = "GCB1HLm-Ii0lfACOjoZmW9qMdjPIKV0pOyBda-DPMec";
 
 	//管理莫尔卡应用的凭证密钥
-	public static String monicaSecret="B6kHFal4F2fIUFscOZP8pDF6QmLGtF06xyZrE_PDdUk";
+	public static String monicaSecret="CGuqFlfBvK0CCHpQVz-DqIpNFqX5vdBWcD7J41A4YPU";
 	//企业应用的idt
-	public static int monicaAgentid=1000011;
+	public static int monicaAgentid=1000002;
 
 	//通讯录应用
-	public static String contactSecret = "gdV_8uxQ_rl9yjQf2Kp0zTHxXbiO8zi2bszC6tOyCmI";
+	public static String contactSecret = "ScREORvyXumlN1YYbzoC7mp-fE0QB30ZXnkkvJ7xGGc";
 	 //应用id
 	public static String agentid = "4";
 	
